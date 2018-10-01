e9ed4a96bf8c00c508ce8b91a2f46d351d7ca786
sikun <arnazadiputra@gmail.com>
2018-10-02 05:42:00 +0700

    change root

--- project/views.py
+++ project/views.py
@@ -4,5 +4,9 @@ from app import app, redirect, scrape, jsonify
 
 
 @app.route("/")
+def hello():
+  return jsonify({"description":"go to /move"})
+
+@app.route("/move")
 def move():
   return redirect('http://go-jek.com/'+scrape()+'/')
